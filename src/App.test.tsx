1b91ce0d9bbed047197b8639a7697af7afc55874
Alisson Martineli <martineli.alisson@gmail.com>
2021-12-10 22:09:12 -0300

    Setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders h1 hello world tag', () => {
+  render(<App />)
+  const h1Element = screen.getByText(/hello world!/i)
+  expect(h1Element).toBeInTheDocument()
+})
